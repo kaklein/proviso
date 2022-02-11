0a5ff717abaa4b3531217ca5b7335bc3fb248d26
Tanisha Matthews <80917634+Mooeyes@users.noreply.github.com>
2022-02-10 21:58:21 -0500

    Add files via upload

---
+++ src/main/login/LoginServel.java
@@ -0,0 +1,87 @@
+package loginone.web;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import loginone.bean.LoginBean;
+import loginone.database.LoginDao;
+
+/**
+ * Servlet implementation class LoginServel
+ */
+@WebServlet("/login")
+public class LoginServel extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServel() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+		String username= request.getParameter("username");
+		String password= request.getParameter("password");
+		
+		LoginBean loginBean = new LoginBean(); 
+		loginBean.setUsername(username);
+		loginBean.setPassword(password);
+		
+		LoginDao loginDao = new LoginDao();
+		
+	
+	
+            if (loginDao.validate(loginBean)) {
+              
+                response.sendRedirect("loginSuccess.jsp");
+            } else {
+                response.sendRedirect("login.jsp");
+              
+            }
+            
+       
+        }
+		
+	}
+	
+
+			
+			
+			
+		
+		
+			
+	
+	
+
+
+
+
+
+
+	
+
+
+
+
